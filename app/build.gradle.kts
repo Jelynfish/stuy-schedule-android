e10a64f535ed87198644845496ec9e239877966c
Jelynfish <thepixeltomato@gmail.com>
2024-05-14 17:21:25 -0400

    Added basic clock logic.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/jelynfish/stuyscheduleapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/jelynfish/stuyscheduleapp/ui/screens/HomeScreen.kt
@@ -1,10 +1,34 @@
 package com.jelynfish.stuyscheduleapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.tooling.preview.Preview
 import com.jelynfish.stuyscheduleapp.ui.UIState
+import kotlinx.coroutines.delay
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 @Composable
 fun HomeScreen(uiState: UIState) {
-    Text("This is the home screen")
+    val currentTime by rememberUpdatedState(newValue = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()))
+
+    LaunchedEffect(key1 = currentTime) {
+        val currentSecond = currentTime.second
+        delay((60 - currentSecond) * 1000L)  // Delay until the next minute
+    }
+    Column {
+        Text("This is the home screen")
+        Text("The time is: ${currentTime.hour}:${currentTime.minute}")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    HomeScreen(uiState = UIState())
 }
