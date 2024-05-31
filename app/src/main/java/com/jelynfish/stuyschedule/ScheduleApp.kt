9cb9ae6712a1da11d4bed2c3b01b2b19b2077ea2
Jelynfish <thepixeltomato@gmail.com>
2024-05-31 17:02:03 -0400

    UI updates, HomeScreen finished.

--- app/src/main/java/com/jelynfish/stuyschedule/ScheduleApp.kt
+++ app/src/main/java/com/jelynfish/stuyschedule/ScheduleApp.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -62,16 +59,6 @@ fun ScheduleApp(
     val uiState by viewModel.uiState.collectAsState()
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(destination.value) },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
-        },
-
         bottomBar = {
             NavigationBar(
                 modifier = Modifier.fillMaxWidth()
